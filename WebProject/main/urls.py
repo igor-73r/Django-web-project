bc184b69f02d5be17906cc35ab47467e760a59cf
github_igorya <igorman.2016@gmail.com>
2023-06-08 21:06:54 +0400

    lab 9 started

--- WebProject/main/urls.py
+++ WebProject/main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('change_table/', views.change_table)
+    path('change_table/', views.change_table),
+    path('edit/<int:id>', views.edit),
+    path('remove/<int:id>', views.remove),
+    path('append/', views.add_new)
 ]
--- WebProject/main/views.py
+++ WebProject/main/views.py
@@ -1,13 +1,45 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import TotalForm
+
 
 def index(request, table=Total):
     fields_name = [f.name for f in table._meta.fields]
-    fields = table._meta.get_fields()
-    print(fields)
+    table_name = table.__name__
     query_results = table.objects.all()
     return render(request, 'main\index.html', locals())
+
+
+def edit(request, id):
+    instance = Total.objects.get(id=id)
+    if request.method == 'POST':
+        form = TotalForm(instance=instance, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = TotalForm(instance=instance)
+    context = {'form': form}
+    return render(request, 'main/edit.html', context)
+
+
+def add_new(request):
+    if request.method == 'POST':
+        form = TotalForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = TotalForm()
+    context = {'form': form}
+    return render(request, 'main/edit.html', context)
+
+
+def remove(request, id):
+    if request.method == 'POST':
+        instance = Total.objects.get(id=id)
+        instance.delete()
+        return redirect('/')
 
 def change_table(request):
     if 'Area' in request.POST:
